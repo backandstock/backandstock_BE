963821e4c393955c59e5203a3916e28f565616f2
yuseonpark <pyuseon@gmail.com>
2022-03-02 12:39:01 +0900

    create project

---
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/MiniBacktestingBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.minibacktesting_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBacktestingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniBacktestingBeApplication.class, args);
+    }
+
+}
---
+++ miniBacktesting_be/src/test/java/com/project/minibacktesting_be/MiniBacktestingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.minibacktesting_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBacktestingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
