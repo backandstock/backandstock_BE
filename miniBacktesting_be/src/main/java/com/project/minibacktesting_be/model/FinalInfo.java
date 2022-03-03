d043068e3db6ed9077aa7d5a0071693091673a3f
nerdved <wjdtjsah12@gmail.com>
2022-03-03 15:26:48 +0900

    pull전 commit finailInfoTest

---
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/model/FinalInfo.java
@@ -0,0 +1,29 @@
+package com.project.minibacktesting_be.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class FinalInfo {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String stockName;
+
+    @Column(nullable = false)
+    private String stockCode;
+
+    @Column(nullable = false)
+    private Long publicDate;
+
+    @Column(nullable = false)
+    private String market;
+
+}
---
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/repository/FinalInfoRepository.java
@@ -0,0 +1,7 @@
+package com.project.minibacktesting_be.repository;
+
+import com.project.minibacktesting_be.model.FinalInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinalInfoRepository extends JpaRepository<FinalInfo, Long> {
+}
