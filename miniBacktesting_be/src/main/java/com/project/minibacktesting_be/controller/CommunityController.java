bdb05eeb3ac3cbb9a4c5ba8da9044344d4add02e
yuseonpark <pyuseon@gmail.com>
2022-03-08 16:04:51 +0900

    community controller 추가

---
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/controller/CommunityController.java
@@ -0,0 +1,11 @@
+package com.project.minibacktesting_be.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CommunityController {
+
+}
