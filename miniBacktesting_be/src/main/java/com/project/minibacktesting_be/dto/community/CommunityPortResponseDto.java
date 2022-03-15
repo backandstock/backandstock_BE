7d9387749efeb2e8ee49cb4b697da446ef8f1957
yuseonPark <pyuseon@gmail.com>
2022-03-15 14:58:42 +0900

    exception처리, likesUser 추가

--- miniBacktesting_be/src/main/java/com/project/minibacktesting_be/dto/community/CommunityPortResponseDto.java
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/dto/community/CommunityPortResponseDto.java
@@ -1,32 +1,20 @@
 package com.project.minibacktesting_be.dto.community;
 
+
 import lombok.*;
+
 import java.util.List;
 
-@Builder
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor // 생성자가 있어야 redis에서 자료를 가져 올 수 있다.
+@AllArgsConstructor
+@Builder
 public class CommunityPortResponseDto {
 
-    Long portId;
-    List<String> stockList;
-    List<Integer> ratioList;
-    Double finalYield;
-    Long seedMoney;
-
-
-    String startDate;
-
-    String endDate;
-
-    List<Double> monthYieldMoney;
-
-    String createdAt;
-
+    CommunityPortDto communityPort;
     Long likesCnt;
     Long commentCnt;
-
+    List<String> likesUsers;
 
 }
--- miniBacktesting_be/src/main/java/com/project/minibacktesting_be/model/Portfolio.java
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/model/Portfolio.java
@@ -49,7 +49,6 @@ public class Portfolio extends Timestamped{
     @OneToMany(mappedBy = "portfolio", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Comment> comments;
 
-
     @JsonManagedReference
     @OneToMany(mappedBy = "portfolio", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<PortStock> portStocks = new ArrayList<>();
