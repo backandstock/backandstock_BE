88119aaccca3d4ccbc3dcc467cc65ffca308832b
nerdved <wjdtjsah12@gmail.com>
2022-03-17 10:32:09 +0900

    전체 댓글 가져오기 ResponseDto에 CreatedAt(생성일자) 추가

--- miniBacktesting_be/src/main/java/com/project/minibacktesting_be/dto/comment/GetCommentsResponseDto.java
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/dto/comment/GetCommentsResponseDto.java
@@ -3,6 +3,8 @@ package com.project.minibacktesting_be.dto.comment;
 import com.project.minibacktesting_be.model.Comment;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +14,7 @@ public class GetCommentsResponseDto {
     private String content;
     private String nickname;
     private Long parentId;
+    private LocalDateTime CreatedAt;
     private List<GetCommentsResponseDto> replyList = new ArrayList<>();
 
     public GetCommentsResponseDto(Comment c) {
@@ -19,5 +22,6 @@ public class GetCommentsResponseDto {
         this.content = c.getContent();
         this.nickname = c.getNickname();
         this.parentId = c.getParentComment().getId();
+        this.CreatedAt = c.getCreatedAt();
     }
 }
