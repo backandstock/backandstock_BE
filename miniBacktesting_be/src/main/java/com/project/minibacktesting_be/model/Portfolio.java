6a61d7af6394929f45a1167fc88635f6d64fad1c
yuseonpark <pyuseon@gmail.com>
2022-03-19 14:11:50 +0900

    portpolio 세터 추가

--- miniBacktesting_be/src/main/java/com/project/minibacktesting_be/model/Portfolio.java
+++ miniBacktesting_be/src/main/java/com/project/minibacktesting_be/model/Portfolio.java
@@ -15,6 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @Entity
 public class Portfolio extends Timestamped{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
