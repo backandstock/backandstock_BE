235da8c76a47d5d5eb4725ac6d1047d18526893f
nerdved <wjdtjsah12@gmail.com>
2022-03-28 12:54:45 +0900

    chore : 파일 경로 리팩토링

--- miniBacktesting_be/src/main/java/com/project/minibacktesting_be/exception/user/S3FileConvertException.java
+++
@@ -1,9 +0,0 @@
-package com.project.minibacktesting_be.exception.user;
-
-public class S3FileConvertException extends RuntimeException{
-    public  S3FileConvertException(String message) {
-
-        super(message);
-    }
-}
-
