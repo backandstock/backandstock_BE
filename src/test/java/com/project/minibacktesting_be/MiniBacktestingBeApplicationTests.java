087972bb9dfdb24e89be46b3003bf83c67d263df
nerdved <wjdtjsah12@gmail.com>
2022-03-28 17:46:09 +0900

    fix : test class public으로 접근제어 변경

--- src/test/java/com/project/minibacktesting_be/MiniBacktestingBeApplicationTests.java
+++ src/test/java/com/project/minibacktesting_be/MiniBacktestingBeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MiniBacktestingBeApplicationTests {
+public class MiniBacktestingBeApplicationTests {
 
     @Test
     public void contextLoads() {
