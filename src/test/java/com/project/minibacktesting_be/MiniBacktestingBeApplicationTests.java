7a29b34cdca5ce8b622185c23d8f119b22dd419f
nerdved <wjdtjsah12@gmail.com>
2022-03-28 17:25:04 +0900

    fix : test함수 contextLoads public으로 접근제어 변경

--- src/test/java/com/project/minibacktesting_be/MiniBacktestingBeApplicationTests.java
+++ src/test/java/com/project/minibacktesting_be/MiniBacktestingBeApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MiniBacktestingBeApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
