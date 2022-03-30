5ed4fd28b0e5e36a3a8ad636dc7bc39a8142b035
nerdved <wjdtjsah12@gmail.com>
2022-03-30 16:38:32 +0900

    fix : nginx 구동 check Controller 내용 변경

---
+++ src/main/java/com/project/minibacktesting_be/controller/nginxController.java
@@ -0,0 +1,22 @@
+package com.project.minibacktesting_be.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nginxController {
+
+    @Value("${logging-module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health")
+    public String checkHealth(){
+        return "healthy";
+    }
+}
